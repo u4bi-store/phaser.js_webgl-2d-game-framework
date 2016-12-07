45c64d981966897815431f536f3afa5709933df6
u4bi <u4bi@u4bi.com>
2016-12-07 04:23:08 +0000

    coin run - render ground, physics init

--- coin-run/js/index.js
+++ coin-run/js/index.js
@@ -1,5 +1,6 @@
 /* coin-run index js*/
 var game;
+var platforms;
 
 function init(){
   game = new Phaser.Game(800, 600, Phaser.AUTO, 'game-area',{
@@ -17,7 +18,20 @@ function preload() {
 }
 
 function create() {
-  game.add.sprite(0, 0, 'star');
+  game.physics.startSystem(Phaser.Physics.ARCADE); /* 물리적용*/
+  game.add.sprite(0, 0, 'sky'); /* 배경 에드*/
+  
+  platforms = game.add.group(); /* 그룹핑함*/
+  platforms.enableBody = true; /* platforms에 들어가는건 물리가 적용됨*/
+  
+  var ground = platforms.create(0, game.world.height -64, 'ground'); /* (width, height, )*/
+  ground.scale.setTo(2, 2); /* size x1, y1 포인터지정*/
+  ground.body.immovable = true; /* 어떠한 충격에 의해 방해를 안받기를 활성화*/
+  
+  var ledge = platforms.create(400, 400, 'ground'); /* 400,400 위치에생성*/
+  ledge.body.immovable = true;
+  ledge = platforms.create(-150, 250, 'ground');
+  ledge.body.immovable = true;
 }
 
 function update() {
