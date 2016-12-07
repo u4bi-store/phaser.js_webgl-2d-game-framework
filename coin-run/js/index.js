0b220d460904104340afec3d1b61148227252e37
u4bi <u4bi@u4bi.com>
2016-12-07 06:08:19 +0000

    star collide , overlap collide callback : collectStar()

--- coin-run/js/index.js
+++ coin-run/js/index.js
@@ -1,6 +1,7 @@
 /* coin-run index js*/
 var game;
 var platforms;
+var player, stars;
 var cursors;
 
 function init(){
@@ -44,10 +45,22 @@ function create() {
   
   player.animations.add('left', [0, 1, 2, 3], 10, true); /* 캐릭터의 애니메이션 정의 왼쪽*/
   player.animations.add('right', [5, 6, 7, 8], 10, true); /* 캐릭터의 애니메이션 정의 오른쪽*/
+  
+  stars = game.add.group();  /* 그룹핑함*/
+  stars.enableBody = true; /* stars에 들어가는건 물리가 적용됨*/
+  
+  for(var i=0; i< 12; i++){ /* 포문을 돌려 별생성*/
+    var star = stars.create(i * 70, 0, 'star'); /*너비는 현재루프 인덱스값 곱하기 70, 높이는 0 위치에 star 생성*/
+    star.body.gravity.y = 6; /* star의 중력도 y값 정의*/
+    star.body.bounce.y = 0.7+Math.random()*0.2; /* star에게 충돌에 의한 튕김값 정의함*/
+  }
 }
 
 function update() {
   var hitPlatform = game.physics.arcade.collide(player, platforms); /* 플레이어와 플랫폼스안의 요소들과 충돌시 감지*/
+  game.physics.arcade.collide(stars, platforms); /* stars그룹이 플랫폼스그룹과 충돌시 감지 팅겨져감*/
+  game.physics.arcade.overlap(player, stars, collectStar, null, this); /* stars그룹이 플레이어와 오버랩될지collectStar 함수 호출*/
+  
   player.body.velocity.x = 0; /* 캐릭터 속력의 x값을 0으로 초기화*/
 
   if(cursors.left.isDown){ /* cursors에 정의된 메서드내의 left를 누른 상태일시*/
@@ -65,4 +78,8 @@ function update() {
     /* 위쪽키를 누른 상태에서 플랫폼과 캐릭터간의 충돌이 감지된 상태에서 그 충돌이 캐릭터의 바디 아래쪽이라면*/
       player.body.velocity.y = -350; /* 속력의 y값을 -350으로 설정함*/
   }
+}
+
+function collectStar (player, star) {
+  star.kill(); /* starts 그룹안의 특정 요소 star를 지움*/
 }
