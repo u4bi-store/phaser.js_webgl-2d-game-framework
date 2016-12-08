131e35a1f538e99f684e3fef317d28da3a45d614
u4bi <u4bi@u4bi.com>
2016-12-08 07:10:46 +0000

    tile game - releaseTile create new tiles

--- tile-game/js/index.js
+++ tile-game/js/index.js
@@ -116,9 +116,9 @@ tileGame.prototype = { /* 클래스 호출됨 prototype에 모두 담음*/
     /**/
   },
   releaseTile: function(){ /* 타일을 눌른 상태에서 때었을 때 호출*/
-    game.input.onUp.remove(this.releaseTile, this); /* 누른 상태에서 때었을 때 해당 함수의 호출을 리무브*/
-    game.input.deleteMoveCallback(this.moveTile, this); /* 무브를 하고 있었을때 호출되는 함수를 삭제함*/
-    game.input.onDown.add(this.pickTile, this); /* 눌렀을 시 해당 핸들러의 콜백함수인 pickTile을 호출함*/
+    game.input.onUp.remove(this.releaseTile, this); /* 때었을 시 이후 해당 함수의 호출 이벤트를 제거함*/
+    game.input.deleteMoveCallback(this.moveTile, this); /* 무브하고 있다 때었을 시 해당함수의 호출 이벤트를 제거함*/
+    game.input.onDown.add(this.pickTile, this); /* 땐 상태에서 다시 눌렀을 때 pickTile 콜백 호출*/
     
     this.arrowsGroup.removeAll(true); /* arrows 그룹 내 모든걸 요소를 리무브함*/
     
@@ -134,7 +134,7 @@ tileGame.prototype = { /* 클래스 호출됨 prototype에 모두 담음*/
         if(this.tilesArray[i][j] != null){
           var holes = this.holesBelow(i, j);
           if(holes > 0){
-            console.log('holes : '+holes);
+            console.log('holes low: '+holes);
             var coordinate = new Phaser.Point(this.tilesArray[i][j].coordinate.x, this.tilesArray[i][j].coordinate.y); /* 최상단 타일 포인터잡아줌*/
             var destination = new Phaser.Point(j, i + holes); /* 리무브되는 타일 포인터잡아줌*/
             console.log("최상단 타일 로: " + coordinate.y + " 컬: " + coordinate.x + "이 이동한곳 로: " + destination.y + " 컬: " + destination.x +'')
@@ -162,9 +162,41 @@ tileGame.prototype = { /* 클래스 호출됨 prototype에 모두 담음*/
         
   		}
   	}
-    game.input.onUp.remove(this.releaseTile, this); /* 때었을 시 이후 해당 함수의 호출 이벤트를 제거함*/
-    game.input.deleteMoveCallback(this.moveTile, this); /* 무브하고 있다 때었을 시 해당함수의 호출 이벤트를 제거함*/
-    game.input.onDown.add(this.pickTile, this); /* 땐 상태에서 다시 눌렀을 때 pickTile 콜백 호출*/
+
+    for(var i = 0; i < game_data.fieldSize; i++){
+      var holes = this.holesInCol(i);
+      if(holes > 0){
+        console.log('holes col: '+holes);
+        for(var j = 1; j <= holes; j++){
+
+          var tileXPos = i * game_data.tileSize + game_data.tileSize / 2; /* 루프가 돌아지는 타일의 x값을 리턴*/
+          var tileYPos = -j * game_data.tileSize + game_data.tileSize / 2; /* 루프가 돌아지는 타일의 y값을 리턴*/
+
+          var theTile = game.add.sprite(tileXPos, tileYPos, "tiles"); /* 타일의 스프라이트 이미지를 지정*/
+          theTile.anchor.set(0.5); /* add한 지정 위치에 대한 x, y 앵커지정 */
+          theTile.picked = false; /* 선택되어지지 않은 타일로 초기화함*/
+          
+          var tween = game.add.tween(theTile).to(
+            { y: theTile.y + holes * game_data.tileSize}, /* theTile의 y와 중첩된 열과 곱해진 타일 사이즈만큼의 y로지정*/
+            game_data.fallSpeed, /*faillSpeed에 정의된 속도만큼*/
+            Phaser.Easing.Linear.None, /* 속성에 걸맞는 이벤트를 줌*/
+            true  /* 트윈 이벤트 활성화*/
+          );
+
+          theTile.coordinate = new Phaser.Point(i, holes - j);
+          this.tilesArray[holes - j][i] = theTile;
+          var text = game.add.text(-game_data.tileSize / 2.8, 0, "로" + theTile.coordinate.y.toString() + ", 컬" + theTile.coordinate.x.toString(), {fill: "#000", font:"bold 30px Arial"});
+           /* 타일내 텍스트 정보 초기화해줌*/
+          
+          console.log("생성된 로: " + theTile.coordinate.y.toString() + " 컬: " + theTile.coordinate.x.toString());
+          console.log("에드됨[" + (holes - j).toString() + "][" + i + "]");
+          theTile.addChild(text);  /* 타일내 텍스트 변경*/
+
+          this.tileGroup.add(theTile);
+        }
+      }
+    } 
+
   },
   checkAdjacent: function(p1, p2){ /*p1과 p2가 서로 인접한지를 체크함*/
     return (Math.abs(p1.x - p2.x) <= 1) && (Math.abs(p1.y - p2.y) <= 1);
@@ -191,9 +223,16 @@ tileGame.prototype = { /* 클래스 호출됨 prototype에 모두 담음*/
   },
   holesBelow: function(row, col){ /* 해당 행의 열중에 null로 초기화된 수를 리턴함 즉 아래로 얼만큼 떨어트릴지를 구하기 위함*/
     var result = 0; /* 0으로 초기화*/
-    for(var i = row + 1; i < game_data.fieldSize; i++){ /* 배치된 행만큼 포문을 돌림*/
+    for(var i = row + 1; i < game_data.fieldSize; i++){ /* 해당 열에서 부터 배치된 행만큼 포문을 돌림*/
       if(this.tilesArray[i][col] == null) result ++; /* 만약 해당 행의 열중에 null이 있다면* 리절트를 쁠쁠해줌*/
     } /*포문이 끝나면*/
     return result; /* 리절트만큼 반환*/
+  },
+  holesInCol: function(col){ /* 해당 열의 행중에 null로 초기화된 수를 리턴함 즉 아래로 얼마의 열이 연달아 떨어질지를 구하기 위함*/
+    var result = 0; /* 0으로 초기화*/
+    for(var i = 0; i < game_data.fieldSize; i++){ /* 모든 열들중에서 배치된 행만큼 포문을 돌림*/
+      if(this.tilesArray[i][col] == null) result ++; /* 만약 해당 행의 열중에 null이 있다면* 리절트를 쁠쁠해줌*/
+    }
+    return result;
   }
 };
