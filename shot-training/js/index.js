f2567dff8651267e47b7ef9935076b21f743bb9a
u4bi <u4bi@u4bi.com>
2016-12-10 13:23:16 +0000

    crosshair render

--- shot-training/js/index.js
+++ shot-training/js/index.js
@@ -2,6 +2,7 @@
 
 var game;
 var target, background;
+var crosshair;
 
 function init(){
   game = new Phaser.Game(1920, 1200, Phaser.AUTO, 'game-area', {
@@ -16,17 +17,27 @@ function preload(){
   game.scale.pageAlignHorizontally = true;
   game.scale.pageAlignVertically = true;
   game.load.image('background', 'images/back_0.jpg');
+  game.load.image('crosshair', 'images/crosshair.png');
   game.load.image('target', 'images/target.png');
 }
 function create(){
   game.physics.startSystem(Phaser.Physics.ARCADE);
   background = game.add.tileSprite(0, 0, 1920, 1200, 'background');
   
-  target = game.add.sprite(100, 100, 'target');
+  target = game.add.sprite(window.innerWidth/2, window.innerHeight/2, 'target');
   game.physics.enable(target, Phaser.Physics.ARCADE);
+  target.anchor.set(-2.2, -0.5);
   target.body.collideWorldBounds = true; 
   target.body.bounce.set(1);
   target.checkWorldBounds = true;
-  target.body.velocity.set(1000, 1000);
+  crosshair = game.add.sprite(window.innerWidth/2, window.innerHeight/2, 'crosshair');
+  
 }
-function update(){}
+function update(){
+  crosshair.x = game.input.x-(crosshair.width/2);
+  crosshair.y = game.input.y-(crosshair.height/2);
+}
+
+function start(){
+  target.body.velocity.set(500, 0);
+}
